0d558f041523fb7da1d46cc6427cedc538629c69
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-22 15:13:02 +0500

    Ishladi.🫡

--- app/db/database.py
+++ app/db/database.py
@@ -1,22 +1,3 @@
-# from sqlalchemy import create_engine, URL
-# from sqlalchemy.ext.declarative import declarative_base
-
-# from app.core.config import config
-
-
-# BATABASE_URL = URL.create(
-#     drivername='postgresql+psycopg2',
-#     host=config.DB_HOST,
-#     port=config.DB_PORT,
-#     username=config.DB_USER,
-#     password=config.DB_PASS,
-#     database=config.DB_NAME
-# )
-
-# engine = create_engine(url=BATABASE_URL)
-# Base = declarative_base()
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
--- app/main.py
+++ app/main.py
@@ -4,21 +4,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.db import models, database
 from app.db.init_db import create_tables
 from app.routers import users
-
-# 🔹 Faqat bitta FastAPI obyekt yaratiladi!
 app = FastAPI(
     title="ITicket API",
     version="0.1.0"
 )
 
-# 🔹 Ma'lumotlar bazasi jadvallarini yaratish
 models.Base.metadata.create_all(bind=database.engine)
 create_tables()
 
-# 🔹 Routerlarni ulash
 app.include_router(users.router)
 
-# 🔹 CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
